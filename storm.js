483cfff5824d33296e4a270c055c9c424408477d
Kate <katshe18@gmail.com>
2024-01-06 11:04:36 +1300

    Working on slider scroll functionality and animation

--- storm.js
+++ storm.js
@@ -1,11 +1,43 @@
-document.getElementById("right-arrow").addEventListener("click", slide);
+document
+  .getElementById("right-arrow")
+  .addEventListener("click", () => slide("right"));
+document
+  .getElementById("left-arrow")
+  .addEventListener("click", () => slide("left"));
 
-function slide() {
+let currentIndex = 0;
+
+function slide(direction) {
+  const leftArrow = document.getElementById("left-arrow");
+  const rightArrow = document.getElementById("right-arrow");
   const slider = document.getElementById("slider");
-  const children = Array.from(slider.children);
-  children.forEach((child) => {
-    if (child.tagName === "IMG") {
-      child.classList.toggle("image-hide");
+  const children = Array.from(slider.children).filter(
+    (child) => child.tagName === "IMG"
+  );
+
+  if (direction === "right") {
+    currentIndex++;
+  } else {
+    currentIndex--;
+  }
+
+  if (currentIndex === 0) {
+    leftArrow.classList.add("hide-arrow");
+    rightArrow.classList.remove("hide-arrow");
+  }
+  if (currentIndex > 0) {
+    leftArrow.classList.remove("hide-arrow");
+  }
+  if (currentIndex === children.length - 1) {
+    rightArrow.classList.add("hide-arrow");
+  }
+
+  children.forEach((child, index) => {
+    if (index === currentIndex) {
+      child.scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+      });
     }
   });
 }
